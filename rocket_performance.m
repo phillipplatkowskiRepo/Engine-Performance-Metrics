deed594877fe88e2008d1bd61a74319d52def2db
phillipplatkowskiRepo <phillipboilermaker@gmail.com>
2026-08-17 12:40:53 -0400

    Rocket Performance Function
    Same as the rocket performance function, but instead recieves inputs and returns parameters in a container.

--- rocket_performance.m
+++ rocket_performance.m
@@ -1,88 +1,106 @@
-clear;
-clc;
+function performance = rocket_performance(Pc, Tc, gamma, R, At, expansion_ratio, Pa)
+% ROCKET_PERFORMANCE
+% Calculates ideal rocket nozzle performance.
+%
+% Inputs:
+%   Pc               Chamber pressure [Pa]
+%   Tc               Chamber temperature [K]
+%   gamma            Specific heat ratio [-]
+%   R                Specific gas constant [J/(kg*K)]
+%   At               Throat area [m^2]
+%   expansion_ratio  Nozzle expansion ratio Ae/At [-]
+%   Pa               Ambient pressure [Pa]
+%
+% Output:
+%   performance      Structure containing calculated performance values
 
-%% Input values here, current R values in metric, can change.
 
-Pc = 10e6;          % Chamber pressure [Pa]
-Tc = 3500;          % Chamber temperature [K]
-gamma = 1.2;        % Specific heat ratio
-R = 355;            % Specific gas constant [J/(kg*K)]
+%% Constants
 
-At = 0.01;          % Throat area [m^2]
-expansion_ratio = 20;
+g0 = 9.80665;       % Standard gravitational acceleration [m/s^2]
+
+
+%% Geometry
+
 Ae = expansion_ratio * At;
 
-Pa = 101325;        % Ambient pressure [Pa]
-g0 = 9.80665;       % Standard gravity [m/s^2]
+
+%% Characteristic Velocity
+
+cstar = sqrt(R * Tc / gamma) / ...
+    (2 / (gamma + 1))^((gamma + 1) / (2 * (gamma - 1)));
 
 
-%% Characteristic velocity
+%% Mass Flow Rate
 
-cstar = sqrt(R*Tc/gamma) / ...
-    (2/(gamma+1))^((gamma+1)/(2*(gamma-1)));
-
-
-%% Mass flow rate
-
-mdot = Pc * At / sqrt(Tc) * sqrt(gamma/R) * ...
-    (2/(gamma+1))^((gamma+1)/(2*(gamma-1)));
+mdot = Pc * At / sqrt(Tc) * sqrt(gamma / R) * ...
+    (2 / (gamma + 1))^((gamma + 1) / (2 * (gamma - 1)));
 
 
-%% Exit Mach number
+%% Exit Mach Number
 
 area_ratio_function = @(M) ...
-    (1/M) * ...
-    ((2/(gamma+1)) * ...
-    (1 + (gamma-1)/2 * M^2))^((gamma+1)/(2*(gamma-1))) ...
+    (1 / M) * ...
+    ((2 / (gamma + 1)) * ...
+    (1 + (gamma - 1) / 2 * M^2))^...
+    ((gamma + 1) / (2 * (gamma - 1))) ...
     - expansion_ratio;
 
 Me = fzero(area_ratio_function, 3);
 
 
-%% Exit temperature
+%% Exit Temperature
 
-Te = Tc / (1 + (gamma-1)/2 * Me^2);
+Te = Tc / ...
+    (1 + (gamma - 1) / 2 * Me^2);
 
 
-%% Exit pressure
+%% Exit Pressure
 
 Pe = Pc * ...
-    (1 + (gamma-1)/2 * Me^2)^(-gamma/(gamma-1));
+    (1 + (gamma - 1) / 2 * Me^2)^...
+    (-gamma / (gamma - 1));
 
 
-%% Exit velocity
+%% Exit Velocity
 
 ae = sqrt(gamma * R * Te);
+
 Ve = Me * ae;
 
 
 %% Thrust
 
 momentum_thrust = mdot * Ve;
+
 pressure_thrust = (Pe - Pa) * Ae;
 
 F = momentum_thrust + pressure_thrust;
 
 
-%% Performance parameters
+%% Specific Impulse
 
 Isp = F / (mdot * g0);
+
+
+%% Thrust Coefficient
 
 Cf = F / (Pc * At);
 
 
-%% Output
+%% Store Results
 
-fprintf('\nRocket Engine Performance Breakdown\n');
+performance.cstar = cstar;
+performance.mdot = mdot;
+performance.Me = Me;
+performance.Te = Te;
+performance.Pe = Pe;
+performance.Ve = Ve;
+performance.momentum_thrust = momentum_thrust;
+performance.pressure_thrust = pressure_thrust;
+performance.F = F;
+performance.Isp = Isp;
+performance.Cf = Cf;
+performance.Ae = Ae;
 
-fprintf('Characteristic velocity: %.2f m/s\n', cstar);
-fprintf('Mass flow rate:          %.3f kg/s\n', mdot);
-fprintf('Exit Mach number:        %.3f\n', Me);
-fprintf('Exit temperature:        %.2f K\n', Te);
-fprintf('Exit pressure:           %.2f kPa\n', Pe/1000);
-fprintf('Exhaust velocity:        %.2f m/s\n', Ve);
-fprintf('Momentum thrust:         %.2f N\n', momentum_thrust);
-fprintf('Pressure thrust:         %.2f N\n', pressure_thrust);
-fprintf('Total thrust:            %.2f N\n', F);
-fprintf('Specific impulse:         %.2f s\n', Isp);
-fprintf('Thrust coefficient:      %.4f\n', Cf);
+end
